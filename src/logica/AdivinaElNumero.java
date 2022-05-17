ef179794322a302cdd8b3c88c64eab73d88dc580
maurocastro29 <mm.castro.29@mail.com>
2022-05-16 19:46:06 -0500

    Se ingresaron algunos mensajes

--- src/logica/AdivinaElNumero.java
+++ src/logica/AdivinaElNumero.java
@@ -5,11 +5,14 @@ public class AdivinaElNumero {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+        //Se crea un numero aleatorio entre 1 y 500
         int numeroAdivinar = (int) (Math.random() * 500 + 1);
         int numero = 0;
+        //Se pide el nombre del usuario
         String nombre = JOptionPane.showInputDialog("Escriba el nombre para el usuario");
         Usuario user = new Usuario(nombre);
+        //Se piden numeros al usuario hasta que encuentre el numero aleatorio
         do{
             try{
                 numero = new Integer(JOptionPane.showInputDialog("Escriba un numero a adivinar"));
@@ -23,6 +26,7 @@ public class AdivinaElNumero {
             }
             user.setIntentos(user.getIntentos() + 1);
         }while(numero != numeroAdivinar);
+        //Se muestra el mensaje
         JOptionPane.showMessageDialog(null, "Felicidades "+user.getNombre()+" el numero ha adivinar era "+numeroAdivinar+"\nlo has conseguido en "+ user.getIntentos()+" intentos");
     }
 }
